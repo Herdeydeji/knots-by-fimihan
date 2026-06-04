41af9969057df9ed0a17c296d74a028f0539333c
Herdeydeji <adedejiheritage26@gmail.com>
2026-06-04 12:57:53 +0100

    Fix missing useEffect import in Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { HiOutlineArrowRight } from 'react-icons/hi'
 import { getFeaturedProducts, heroImage } from '../lib/products'
